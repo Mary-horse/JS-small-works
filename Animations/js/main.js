ba50c46ebb8bf91749b7ee7f294432e74933f5a2
Mary-horse <75565785+Mary-horse@users.noreply.github.com>
2021-06-09 20:28:05 +0300

    Add files via upload

---
+++ Animations/js/main.js
@@ -0,0 +1,24 @@
+// $('.btn').click(function () {
+//   $('.btn').addClass('color_btn');
+//   $('.btn').removeClass('.color_btn');
+// });
+
+$('.btn1').click(function () {
+  $('.block_add1').toggleClass('hide');
+});
+
+$('.btn2').click(function () {
+  $('.block_add2').toggleClass('color_add');
+});
+
+$('.btn3').click(function () {
+  $('.block_add3').toggleClass('run');
+});
+
+$('.btn4').click(function () {
+  $('.block_add4').toggleClass('circle');
+});
+
+$('.btn5').click(function () {
+  $('.block_add5').toggleClass('rotFun');
+});
